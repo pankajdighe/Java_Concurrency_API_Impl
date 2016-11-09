0381df10e9a08f311bd47ac4c4482a5f8cad561f
pankaj_dighe <pankajdighe77@gmail.com>
2016-11-08 20:21:17 -0800

    Test comment

--- src/main/java/ReentrantLockDemo.java
+++ src/main/java/ReentrantLockDemo.java
@@ -3,7 +3,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * Created by pankaj on 11/8/16.
  *
- * This is simple implementation of ReentrantLick
+ * This is simple implementation of ReentrantLock
  * This demo will start three threads, all threads print the specific string on console
  * This program will synchronize the these three threads
  *
